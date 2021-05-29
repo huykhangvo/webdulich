fe470b3e2a1c323ca7d4345912d6c694bf7a098e
Võ Huy Khang <47880923+huykhangvo@users.noreply.github.com>
2021-05-29 10:42:34 +0700

    Update ketnoi.php

--- db/ketnoi.php
+++ db/ketnoi.php
@@ -5,6 +5,6 @@
 
 <?php
 //$con=mysqli_connect("localhost","root","mysql","dulich");
-$con=mysqli_connect("freedb.tech","freedbtech_nhutdong","pha","freedbtech_khamphadulich");
+$con=mysqli_connect("freedb.tech","freedbtech_nhutdong","kham-pha-du-lich","freedbtech_khamphadulich");
 
 ?>
